953122fd171e7283f59e2ed1b69cd35be968396c
wang-yurin <king_1994@naver.com>
2023-04-21 14:50:22 +0900

    feat: MessageInput 컴포넌트 API 연동 - 전송 버튼을 클릭하면 POST 요청하여 데이터를 받아옵니다.

--- src/components/MessageInput/MessageInput.tsx
+++ src/components/MessageInput/MessageInput.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Col, Input, Row } from 'antd';
 import { SendOutlined } from '@ant-design/icons';
+import createChatApi from '../../api/createChat';
 
 const { TextArea } = Input;
 
 const MessageInput = () => {
+  const [userMessage, setUserMessage] = useState<string>('');
+
+  const handleTypingMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setUserMessage(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    const requestData = {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: userMessage }],
+    };
+
+    createChatApi(requestData);
+  };
+
   return (
     <Row align="bottom" gutter={16}>
       <Col flex={1}>
-        <TextArea showCount allowClear={true} />
+        <TextArea
+          showCount
+          allowClear={true}
+          onChange={handleTypingMessage}
+          value={userMessage}
+        />
       </Col>
-      <Button size="large" icon={<SendOutlined />}>
+      <Button size="large" icon={<SendOutlined />} onClick={handleSendMessage}>
         전송
       </Button>
     </Row>
