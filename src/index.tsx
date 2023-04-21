61ce90fe79619ff26733e5fef9790fe085b21c76
wang-yurin <king_1994@naver.com>
2023-04-21 18:02:50 +0900

    feat: ChatPage

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from '@emotion/react';
+import theme from './styles/theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/ChatPage/ChatPage.styles.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const contentStyle = {
+  padding: '2rem',
+};
+
+const siderStyle = {
+  padding: '1rem',
+};
+
+export { contentStyle, siderStyle };
