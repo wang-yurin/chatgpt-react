6167ee8e12ee09f3050370ab339f0ecc02245525
wang-yurin <king_1994@naver.com>
2023-04-24 22:39:47 +0900

    feat: MessageInput 컴포넌트 기능 구현 및 스타일 적용

---
+++ src/components/MessageInput/MessageInput.jsx
@@ -0,0 +1,27 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { Button, Col, Input, Row } from 'antd';
+import { SendOutlined } from '@ant-design/icons';
+import messageInputStyles from './MessageInput.styles';
+
+const { TextArea } = Input;
+
+const MessageInput = ({ value, onChange, onSend }) => {
+  return (
+    <Row align="bottom" gutter={16} css={messageInputStyles}>
+      <Col flex={1}>
+        <TextArea
+          showCount
+          allowClear={true}
+          onChange={onChange}
+          value={value}
+        />
+      </Col>
+      <Button size="large" icon={<SendOutlined />} onClick={onSend}>
+        전송
+      </Button>
+    </Row>
+  );
+};
+
+export default MessageInput;
---
+++ src/components/MessageInput/MessageInput.styles.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react';
+import theme from '../../styles/theme';
+
+const messageInputStyles = css`
+  padding: 5rem 0;
+  border-top: 1px solid ${theme.colors.GREY300};
+`;
+
+export default messageInputStyles;
